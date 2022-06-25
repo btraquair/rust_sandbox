218e491e1494ba175bd9cb088a89250202bbf9aa
Benjamin Traquair <btraquair@bristlecone.(none)>
2022-06-25 10:21:23 -0600

    initial

---
+++ hello_world/hello.rs
@@ -0,0 +1,12 @@
+// Comment style
+
+/*
+ * Wow this is a block comment
+ */
+
+// Main Function
+fn main() {
+    print!("hello world");
+    println!("I'm a rustacean");
+    println!("Fun with numbers. {0}, {0:X}, {0:b}", 12345);
+}
